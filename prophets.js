51b8a9af600ed0569e172e3897156c59f35d5752
jsanchezb34 <jsanchezb34@>
2025-11-05 09:21:54 -0400

    js complete

--- prophets.js
+++ prophets.js
@@ -1,33 +1,37 @@
 const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
-async function getProphetData(){
-    const response = await fetch("https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json");
-    const data = await response.json();
-    console.table(data.prophets);
-}
-getProphetData();
+
+
 
 const displayProphets = (prophets) => {
-    prophets.forEach(prophet) => {
+    prophets.forEach((prophet) => {
 
         const card = document.createElement('section');
-        const h2 =document.childElement('h2');
+        const h2 =document.createElement('h2');
         const birth = document.createElement('p');
         const place = document.createElement('p')
         const picture = document.createElement('img');
-    
+        
 
-    h2.textContent = "${prophet.name} $prophet.lastname";
-    birth.textContent ="${birthdate}";
-    place.textContent = "${birthplace}";
-    picture.setAtributte ('src', prophet.imageurl);
-    picture.setAtributte ('alt', 'portrait of ${prophet.name} ${prophet.lastname}');
-    picture.setAtributte ('loading', 'lazy'); 
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        birth.textContent = `Birthdate: ${prophet.birthdate}`;
+        place.textContent = `Birthplace: ${prophet.birthplace}`;
+        picture.setAttribute('src', prophet.imageurl);
+        picture.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        picture.setAttribute('loading', 'lazy');
 
-    card.appendChild();
-    card.appendChild();
-    card.appendChild();
-    card.appendChild();
-    card.appendChild();
-});
+        card.appendChild(h2);
+        card.appendChild(birth);
+        card.appendChild(place);
+        card.appendChild(picture);
+        cards.appendChild(card);
+    });
 };
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData();
