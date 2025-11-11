e0e061b4b392b877d43795117f8d205e8a8e971e
jsanchezb34 <jsanchezb34@>
2025-11-11 14:19:46 -0400

    week3 learning activities

---
+++ scripts/output.mjs
@@ -0,0 +1,7 @@
+function setTitle(course) {
+  document.querySelector("#courseName").textContent = course.name;
+  document.querySelector("#courseCode").textContent = course.code;
+}
+
+export function setTitle (course) {}
+export function renderSections (sections) { }
