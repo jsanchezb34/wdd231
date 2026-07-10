48e1aa5571c8165c610d04b2c2310c290bc0014c
jsanchezb34 <jsanchezb34@>
2026-07-10 16:48:36 -0400

    asdfg

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,2 +1,4 @@
 const today =new Date();
 console.log(today.toDateString());
+document.getElementById("lastModified").textContent = document.lastModified;
+document.getElementById("currentYear").textContent = new Date().getFullYear();
